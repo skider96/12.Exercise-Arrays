ce9b3dd5707477c4db580ec7334b86b0f661e60c
skider96 <danailqkiq@gmail.com>
2023-10-15 18:58:50 +0300

    problems solved

--- 01. Train/Program.cs
+++ 01. Train/Program.cs
@@ -4,25 +4,26 @@
     {
         static void Main(string[] args)
         {
-           int numberOfWagons = int.Parse(Console.ReadLine);
-           
- int sum = 0;
-           
-           int[] people = new int [numberOfWagons];
-           for (var i = 0; i < wagons.Length - 1; i++)
-           {
-            int numberOfPeople = int.Parse(Console.ReadLine);
-             sum += numberOfPeople;
-            people[i] = numberOfPeople;
-           }
-        
-         
-           for (var i = 0; i < people.Length; i++)
-           {
-            System.Console.Write($"{people[i]} ");
+            int numberOfWagons = int.Parse(Console.ReadLine());
 
-           }
-           System.Console.WriteLine(sum);
+            int sum = 0;
+
+            int[] people = new int[numberOfWagons];
+            for (var i = 0; i < numberOfWagons; i++)
+            {
+                int numberOfPeople = int.Parse(Console.ReadLine());
+                sum += numberOfPeople;
+                people[i] = numberOfPeople;
+            }
+
+
+            for (var i = 0; i < people.Length; i++)
+            {
+                System.Console.Write($"{people[i]} ");
+    
+            }
+            System.Console.WriteLine();
+            System.Console.WriteLine(sum);
         }
     }
 }
--- 02.CommonElements/Program.cs
+++ 02.CommonElements/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] firstLine = Console.ReadLine().Split().ToArray();
+            string[] secondLine = Console.ReadLine().Split().ToArray();
+
+            for (var i = 0; i < firstLine.Length; i++)
+            {
+                for (var j = 0; j < secondLine.Length; j++)
+                {
+                    if (firstLine[i] == secondLine[j])
+                    {
+                        System.Console.Write(secondLine[j] + " ");
+                    }
+                    
+                }
+            }
+            // Hey hello 2 4
+            // 10 hey 4 hello
         }
     }
 }
