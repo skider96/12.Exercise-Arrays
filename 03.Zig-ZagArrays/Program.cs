ef84fdc859e5e686512f100235ea120fa8c86f53
skider96 <danailqkiq@gmail.com>
2023-10-16 18:05:05 +0300

    problems solved

--- 03.Zig-ZagArrays/Program.cs
+++ 03.Zig-ZagArrays/Program.cs
@@ -1,10 +1,39 @@
 ﻿namespace _03.Zig_ZagArrays
 {
-    internal class Program
+  internal class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      int n = int.Parse(Console.ReadLine());
+      int[] firstDigits = new int[n];
+      int[] secondDigits = new int[n];
+      string[] input = new string[1];
+
+      for (var i = 0; i < n; i++)
+      {
+        input = Console.ReadLine().Split();
+        if (i % 2 == 0)
         {
-            Console.WriteLine("Hello, World!");
+          firstDigits[i] = int.Parse(input[0]);
+          secondDigits[i] = int.Parse(input[1]);
         }
+        else
+        {
+          secondDigits[i] = int.Parse(input[0]);
+          firstDigits[i] = int.Parse(input[1]);
+        }
+
+      }
+      System.Console.WriteLine(string.Join(" ", firstDigits));
+      System.Console.WriteLine(string.Join(" ", secondDigits));
     }
+  }
 }
+/*
+4
+1 5
+9 10
+31 81
+41 20
+
+*/
