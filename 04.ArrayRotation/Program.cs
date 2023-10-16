47402d011b49c04c26929598e75c6da85f97fd17
skider96 <danailqkiq@gmail.com>
2023-10-16 19:27:57 +0300

    problem solved

--- 04.ArrayRotation/Program.cs
+++ 04.ArrayRotation/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] text = Console.ReadLine().Split();
+
+            int[] numbers = Array.ConvertAll(text, int.Parse);
+            int rotations = int.Parse(Console.ReadLine());
+
+            for (var i = 0; i < rotations; i++)
+            {
+                int firstElement = numbers[0];
+                for (var j = 0; j < numbers.Length - 1; j++)
+                {
+                    numbers[j] = numbers[j + 1];
+                }
+
+                numbers[numbers.Length - 1] = firstElement;
+
+            }
+
+            System.Console.WriteLine(string.Join(" ", numbers));
+            //направено с chatGPT 
         }
     }
 }
