16766042f7ecfc0409ba43a8cafe9c05633f2dfd
skider96 <danailqkiq@gmail.com>
2023-10-16 20:32:46 +0300

    fuck update

--- 05.TopIntegers/Program.cs
+++ 05.TopIntegers/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Create a program to find all the top integers in an array. A top integer is an
+            // integer that is greater than all the
+            //elements to its right.
+
+            string[] input = Console.ReadLine().Split();
+            int[] number = Array.ConvertAll(input, int.Parse);
+
+            for (int i = number.Length - 1; i >= 0 ; i--)
+            {
+              int maxNumber = number.Max();
+                if (number[i] < number[i -1])
+                {
+
+                    System.Console.Write(string.Join("", number[i]));
+                    System.Console.Write(" ");
+                }  
+            }
+            
+           
+            System.Console.Write(string.Join(" ", number[number.Length - 1]));
         }
     }
 }
