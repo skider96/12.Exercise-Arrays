08cfcca01234fca7da137f71b144ac4c24fffe07
skider96 <danailqkiq@gmail.com>
2023-10-17 14:29:48 +0300

    problem solved

--- 06.EqualSum/Program.cs
+++ 06.EqualSum/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] input = Console.ReadLine().Split();
+            int[] numbers = Array.ConvertAll(input, int.Parse);
+            int sumIncrease = 0;
+            int sumDecrease = 0;
+            bool isText = false;
+            foreach (int item in numbers)
+            {
+                sumIncrease += item;
+            }
+            for (var i = 0; i < numbers.Length - 1; i++)
+            {
+                sumIncrease -= numbers[i];
+                if (sumIncrease == sumDecrease)
+                {
+
+                    System.Console.WriteLine(i);
+                    isText = true;
+                }
+
+
+                sumDecrease += numbers[i];
+            }
+            if (numbers.Length - 1 == 0)
+            {
+                System.Console.WriteLine(0);
+            }
+            else if (isText == false)
+            {
+                System.Console.WriteLine("no");
+            }
+
+
         }
     }
 }
